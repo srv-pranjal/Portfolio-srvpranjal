43a408e6682224ef0bbeb9a18dc9457d374bb9c4
pranjal.s <pranjal.s@highradius.com>
2021-07-19 00:44:20 +0530

    docs: skill section added

--- App.js
+++ App.js
@@ -9,7 +9,35 @@ function openHome(){
     document.getElementById("secondary-nav-bar").style.display="none";
     document.getElementById("about").style.display="none";
 }
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
 
+const scrollto = (el) => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    console.log("ss")
+    console.log(typed_strings)
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
 // function openProjects(){
 //     document.getElementById("home").style.display="none";
 //     document.getElementById("secondary-nav-bar").style.display="flex";
