6bd5b6dc81d205fa0801f94f38f9f4c34422e257
pranjal.s <pranjal.s@highradius.com>
2021-07-24 23:59:28 +0530

    update: resume page added

--- App.js
+++ App.js
@@ -1,60 +1,28 @@
-function openAbout(){
-    document.getElementById("home").style.display="none";
-    document.getElementById("secondary-nav-bar").style.display="flex";
-    document.getElementById("about").style.display="block";
-}
 
 function openHome(){
-    document.getElementById("home").style.display="flex";
-    document.getElementById("secondary-nav-bar").style.display="none";
-    document.getElementById("about").style.display="none";
+    handlePages("home");
+    makeActive("nav-home");
 }
-const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
 
-const scrollto = (el) => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
-  }
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    console.log("ss")
-    console.log(typed_strings)
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
-// function openProjects(){
-//     document.getElementById("home").style.display="none";
-//     document.getElementById("secondary-nav-bar").style.display="flex";
-//     document.getElementById("about").style.display="none";
-// }
+function openAbout(){
+  handlePages("about");
+  makeActive("nav-about");
+}
 
-// function openResume(){
-//     document.getElementById("home").style.display="none";
-//     document.getElementById("secondary-nav-bar").style.display="flex";
-//     document.getElementById("about").style.display="none";
-// }
+function openProjects(){
+    handlePages("projects");
+    makeActive("nav-projects")
+}
 
-// function openContact(){
-//     document.getElementById("home").style.display="none";
-//     document.getElementById("secondary-nav-bar").style.display="flex";
-//     document.getElementById("about").style.display="none";
-// }
+  function openResume(){
+    handlePages("resume");
+    makeActive("nav-resume");
+}
+
+function openContact(){
+    handlePages("contact");
+    makeActive("nav-contact");
+}
 
 // function openMobileNav() {
 //     var x = document.getElementById("mobile-nav");
@@ -66,3 +34,31 @@ const scrollto = (el) => {
 //     document.getElementById("home").style.display="none";
 //     document.getElementById("about").style.display="none";
 //   }
+
+function makeActive(navID){
+  var navElements = document.getElementsByClassName("link-primary");
+  for(var x=0;x<navElements.length;x++)
+  {
+    if(navElements[x].classList.contains("active")){
+      navElements[x].classList.remove("active");
+    }
+  }
+  var element = document.getElementById(navID);
+  element.classList.add("active");
+}
+
+function handlePages(pageID){
+  document.getElementById("home").style.display="none";
+  document.getElementById("about").style.display="none";
+  document.getElementById("resume").style.display="none";
+  if(pageID=="home")
+  {
+    document.getElementById("secondary-nav-bar").style.display="none";
+    document.getElementById(pageID).style.display="flex";
+  }
+  else
+  {
+    document.getElementById("secondary-nav-bar").style.display="flex";
+    document.getElementById(pageID).style.display="block";
+  }
+}
