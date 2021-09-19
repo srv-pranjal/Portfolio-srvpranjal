dbb33b04c2195f33d1c63ab0b309a346dd4b5d08
srv-pranjal <prns366@gmail.com>
2021-09-20 02:24:15 +0530

    update: refactor selectors

--- App.js
+++ App.js
@@ -1,78 +1,82 @@
+const homePage = document.querySelector("#home");
+const aboutPage = document.querySelector("#about");
+const resumePage = document.querySelector("#resume");
+const projectsPage = document.querySelector("#projects");
+const blogsPage = document.querySelector("#blogs");
+const contactPage = document.querySelector("#contact");
+const mobileNavBar = document.querySelector("#mobile-nav");
+const secondaryNavBar = document.querySelector("#secondary-nav-bar");
+const navElements = document.querySelectorAll(".link-primary");
+
 function openPage(pageId, navId) {
   handlePages(pageId);
   makeActive(navId);
-  burgerButton = document.querySelector(".fa-times");
-  burgerButton.classList.remove("fa-times");
-  burgerButton.classList.add("fa-bars");
+  let burgerButton = document.querySelector(".fa-times");
+  if (burgerButton != null) {
+    burgerButton.classList.remove("fa-times");
+    burgerButton.classList.add("fa-bars");
+  }
 }
 
 function openMobileNav() {
-  var x = document.querySelector("#mobile-nav");
   var currNavId = document.querySelector(".active");
   var currPageId = currNavId.innerHTML.toLowerCase();
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-    burgerButton = document.querySelector(".fa-times");
+  if (mobileNavBar.style.display === "flex") {
+    mobileNavBar.style.display = "none";
+    let burgerButton = document.querySelector(".fa-times");
     burgerButton.classList.remove("fa-times");
     burgerButton.classList.add("fa-bars");
     handlePages(currPageId);
   } else {
-    x.style.display = "flex";
-    burgerButton = document.querySelector(".fa-bars");
+    mobileNavBar.style.display = "flex";
+    let burgerButton = document.querySelector(".fa-bars");
     burgerButton.classList.remove("fa-bars");
     burgerButton.classList.add("fa-times");
-    document.querySelector("#home").style.display = "none";
-    document.querySelector("#about").style.display = "none";
-    document.querySelector("#resume").style.display = "none";
-    document.querySelector("#projects").style.display = "none";
-    document.querySelector("#blogs").style.display = "none";
-    document.querySelector("#contact").style.display = "none";
+    hidePages();
   }
+}
+
+function hidePages() {
+  homePage.style.display = "none";
+  aboutPage.style.display = "none";
+  resumePage.style.display = "none";
+  projectsPage.style.display = "none";
+  blogsPage.style.display = "none";
+  contactPage.style.display = "none";
 }
 
 function makeActive(navID) {
-  var navElements = document.getElementsByClassName("link-primary");
-  for (var x = 0; x < navElements.length; x++) {
-    if (navElements[x].classList.contains("active")) {
-      navElements[x].classList.remove("active");
+  for (let index = 0; index < navElements.length; index++) {
+    if (navElements[index].classList.contains("active")) {
+      navElements[index].classList.remove("active");
     }
   }
-  var element = document.getElementById(navID);
+  const element = document.getElementById(navID);
   element.classList.add("active");
-  scrollto(element);
+  scrollto();
 }
 
 function handlePages(pageID) {
-  document.querySelector("#home").style.display = "none";
-  document.querySelector("#about").style.display = "none";
-  document.querySelector("#resume").style.display = "none";
-  document.querySelector("#projects").style.display = "none";
-  document.querySelector("#blogs").style.display = "none";
-  document.querySelector("#contact").style.display = "none";
-  document.querySelector("#mobile-nav").style.display = "none";
+  hidePages();
+  const currPage = document.getElementById(pageID);
+  mobileNavBar.style.display = "none";
   if (pageID == "home") {
-    document.querySelector("#secondary-nav-bar").style.display = "none";
-    document.getElementById(pageID).style.display = "flex";
+    secondaryNavBar.style.display = "none";
+    currPage.style.display = "flex";
   } else {
-    document.querySelector("#secondary-nav-bar").style.display = "flex";
-    document.getElementById(pageID).style.display = "block";
+    secondaryNavBar.style.display = "flex";
+    currPage.style.display = "block";
   }
 }
 
-function onSubmit() {
+function onSubmit(e) {
+  e.preventDefault();
   document.querySelector("#form").submit();
-  console.log("form submitted");
 }
 
-const scrollto = (el) => {
+const scrollto = () => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 };
-
-function animateSkills() {
-  var element = document.querySelector(".cplusplus");
-  console.log(element);
-  element.style.backgroundColor = "red";
-}
